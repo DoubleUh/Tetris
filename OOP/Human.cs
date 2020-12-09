ae836c41843f427b43a6e8e9cd87085ab1d76f2e
VlVinichenko <vlvinichenko@gmail.com>
2020-12-09 07:15:41 +0200

    урок 3.7.1

---
+++ OOP/Human.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    abstract class Human
+    {
+        public int age;
+        public int height;
+
+        public void Set(int age)
+        {
+            this.age = age;
+        }
+        public void Set(int age, int height)
+        {
+            this.age = age;
+            this.height = height;
+        }
+
+        //public virtual void Write()
+        //{
+        //    Console.WriteLine($"age: {age}, height: {height}");
+        //}
+
+        public abstract void Write();
+
+    }
+}
---
+++ OOP/Officer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Officer : Human
+    {
+        public string rank;
+
+        public Officer(int age, string rank)
+        {
+            this.age = age;
+            this.rank = rank;
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine($"age: {age}, height: {height}, rank: {rank}");
+        }
+
+    }
+
+
+}
